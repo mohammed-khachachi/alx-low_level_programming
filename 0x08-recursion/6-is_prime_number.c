b248d11379d5e1b9d3ca8c9e80f15617544691f0
mohammed_khachachi <khachachivscod@gmail.com>
2023-04-05 19:25:12 -0700

    6-is_prime_number.c :|

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,15 +15,3 @@ int check(int a, int b)
 	else
 		return (check(a + 1, b));
 }
-
-/**
- * is_prime_number - states if number is prime
- * @n:int
- * Return:int
- */
-int is_prime_number(int n)
-{
-	if (n == 2)
-		return (1);
-	return (check(2, n));
-}
